566796b13f8e38786b6f3b0953d8f0d84b47c72c
mac 5 <KristalPistol@kristalpistols-macbook-pro.local>
2016-06-14 08:53:09 -0700

    Initial Commit

---
+++ Libimobile/Libimobile.h
@@ -0,0 +1,19 @@
+//
+//  Libimobile.h
+//  Libimobile
+//
+//  Created by KristalPistol on 6/14/16.
+//  Copyright © 2016 KristalPistol. All rights reserved.
+//
+
+#import <Cocoa/Cocoa.h>
+
+//! Project version number for Libimobile.
+FOUNDATION_EXPORT double LibimobileVersionNumber;
+
+//! Project version string for Libimobile.
+FOUNDATION_EXPORT const unsigned char LibimobileVersionString[];
+
+// In this header, you should import all the public headers of your framework using statements like #import <Libimobile/PublicHeader.h>
+
+
